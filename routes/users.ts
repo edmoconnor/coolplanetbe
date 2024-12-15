f66ce8a40897c53888a2371d83a024d75602b35b
Ed O'Connor <edmoconnor@gmail.com>
2024-12-15 19:34:10 +0000

    tidy up

--- routes/users.ts
+++ routes/users.ts
@@ -4,23 +4,18 @@ const data = require('../users.json')
 
 /* GET users listing. */
 router.get('/', (req: Request, res: Response) => {
-  console.log('id')
   res.json(data);
 });
 
 router.get('/:id', (req: Request, res: Response) => {
-  console.log('dfsvserdbvsredf')
   const id  = req.params.id;
-  console.log(id)
-  var user;
+  let userDetails: any = {};
   for(var i = 0; i<data.length; i++){
     if(data[i].id == id){
-      user = data[i];
-      console.log('data' + JSON.stringify(user.first_name))
+      userDetails = data[i];
     }
   }
-  
-  res.json(user);
+  res.json(userDetails);
 });
 
 module.exports = router;
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,17 @@
-import createError from 'http-errors';
-// import morgan from 'morgan';
 import express, { Request, Response, NextFunction} from 'express';
 import path from 'path';
-// import cookieParser from 'cookie-parser';
-import logger from 'jet-logger';
 import cors from "cors";
-
 
 const indexRouter = require('../routes/index');
 const usersRouter = require('../routes/users');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
-
-// view engine setup
-const viewsDir = path.join(__dirname, 'views');
-app.set('view engine', 'jade');
 app.use(cors());
-// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '../../react/coolplanetfe/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -33,19 +21,12 @@ app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
   });
 
-//catch 404 and forward to error handler
-// app.use(function(req: Request, res: Response) => {
-//   next(createError(404));
-// });
-
-// // error handler
+// error handler
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
-//   res.status(err.status || 500);
   res.render('error');
 });
 
