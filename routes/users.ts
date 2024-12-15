845cffdb8917d43be0c17176de76f1d93a67e463
Ed O'Connor <edmoconnor@gmail.com>
2024-12-15 21:57:20 +0000

    revert ( bad id error broke )

--- routes/users.ts
+++ routes/users.ts
@@ -4,18 +4,20 @@ const data = require('../users.json')
 
 /* GET users listing. */
 router.get('/', (req: Request, res: Response) => {
+  console.log('id')
   res.json(data);
 });
 
 router.get('/:id', (req: Request, res: Response) => {
   const id  = req.params.id;
-  let userDetails: any = {};
+  var user;
   for(var i = 0; i<data.length; i++){
     if(data[i].id == id){
-      userDetails = data[i];
+      user = data[i];
     }
   }
-  res.json(userDetails);
+  
+  res.json(user);
 });
 
 module.exports = router;
